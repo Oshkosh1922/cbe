e6c3a8539851bb18ba330129d2cab65ff20f5b8a
Michael Harris <oshkosh1922@gmail.com>
2024-09-03 12:28:04 -0500

    fg
    fgfg

--- js/task.js
+++ js/task.js
@@ -216,3 +216,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
    
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const submissionLinks = document.querySelectorAll('.submission-link');
+
+    submissionLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            localStorage.setItem('scrollPosition', window.scrollY);
+        });
+    });
+
+    const savedPosition = localStorage.getItem('scrollPosition');
+    if (savedPosition) {
+        window.scrollTo(0, parseInt(savedPosition));
+        localStorage.removeItem('scrollPosition');
+    }
+});
